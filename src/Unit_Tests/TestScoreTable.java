9c24023b7b8653d16fcc584b939c44a89a207e0a
Sergiy Babenkov <sergiy.babenkov@uconn.edu>
2022-10-05 14:24:40 -0400

    Initial commit

--- src/Unit_Tests/TestScoreTable.java
+++ src/Unit_Tests/TestScoreTable.java
@@ -5,8 +5,9 @@ import org.testng.annotations.Test;
 import java.sql.*;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 
-class TestScoreTable {
+public class TestScoreTable {
 
     static final String JDBC_DRIVER = "org.h2.Driver";
     static final String DB_URL = "jdbc:h2:~/test";
@@ -16,39 +17,98 @@ class TestScoreTable {
 
 
     @Test
-    void test() {
+    public void test() {
         Connection conn = null;
         Statement stmt = null;
         try {
-            // STEP 0: Register JDBC driver
+            // STEP 1: Register JDBC driver
             Class.forName(JDBC_DRIVER);
 
-            //STEP 0: Open a connection
+            // STEP 2: Open a connection
+            System.out.println("Connecting to a selected database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
             /* */
-            //STEP 1: Insert a test row in the score table
+            //STEP 3: Insert a test row in the score table
 
             stmt = conn.createStatement();
-            String sql = "INSERT INTO  score " +
+            String sql =  "INSERT INTO  score " +
                     "(usr,questions_total,questions_answered,questions_correct) " +
-                    "VALUES ('Alan Turing', 100, -5, 2); ";
+                    "VALUES ('Sergiy Babenkov', 100, -5, 2); ";
             stmt.executeUpdate(sql);
+
+            sql =  "INSERT INTO  score " +
+                    "(usr,questions_total,questions_answered,questions_correct) " +
+                    "VALUES ('Nadia Brown', 50, 45, 30); ";
+            stmt.executeUpdate(sql);
+
+            sql =  "INSERT INTO  score " +
+                    "(usr,questions_total,questions_answered,questions_correct) " +
+                    "VALUES ('Matthew Dunn', 26, 14, 5); ";
+            stmt.executeUpdate(sql);
+
+            System.out.println("Inserted records into the table...");
             /* */
 
-            // STEP 2: Select the rest row
-
+            // STEP 4 Select the row
             stmt = conn.createStatement();
-            sql = "SELECT * FROM score WHERE usr='Alan Turing' ;";
+
+            sql = "SELECT * FROM score WHERE usr='Sergiy Babenkov';";
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
-                String actualString = rs.getString("questions_total");
-                int actualInt = Integer.parseInt(actualString);
-                assertEquals(actualInt, 100);
+                String questionsTotal = rs.getString("questions_total");
+                int TotalInt = Integer.parseInt(questionsTotal);
+                assertEquals(TotalInt, 100);
+
+                String questionsAnswered = rs.getString("questions_answered");
+                int AnsweredInt = Integer.parseInt(questionsAnswered);
+                assertEquals(AnsweredInt, -5);
+
+                String questionsCorrect = rs.getString("questions_correct");
+                int CorrectInt = Integer.parseInt(questionsCorrect);
+                assertEquals(CorrectInt, 2);
             }
 
-            //STEP 3: Remove the test row
-            sql = "DELETE FROM score WHERE usr='Alan Turing' ;";
+            sql = "SELECT * FROM score WHERE usr='Nadia Brown';";
+            ResultSet rs2 = stmt.executeQuery(sql);
+            while (rs2.next()) {
+                String questionsTotal = rs2.getString("questions_total");
+                int TotalInt = Integer.parseInt(questionsTotal);
+                assertEquals(TotalInt, 50);
+
+                String questionsAnswered = rs2.getString("questions_answered");
+                int AnsweredInt = Integer.parseInt(questionsAnswered);
+                assertEquals(AnsweredInt, 45);
+
+                String questionsCorrect = rs2.getString("questions_correct");
+                int CorrectInt = Integer.parseInt(questionsCorrect);
+                assertEquals(CorrectInt, 30);
+            }
+
+            sql = "SELECT * FROM score WHERE usr='Matthew Dunn';";
+            ResultSet rs3 = stmt.executeQuery(sql);
+            while (rs3.next()) {
+                String questionsTotal = rs3.getString("questions_total");
+                int TotalInt = Integer.parseInt(questionsTotal);
+                assertEquals(TotalInt, 26);
+
+                String questionsAnswered = rs3.getString("questions_answered");
+                int AnsweredInt = Integer.parseInt(questionsAnswered);
+                assertEquals(AnsweredInt, 14);
+
+                String questionsCorrect = rs3.getString("questions_correct");
+                int CorrectInt = Integer.parseInt(questionsCorrect);
+                assertEquals(CorrectInt, 5);
+            }
+
+            //STEP 5: Remove the test row
+            sql = "DELETE FROM score WHERE usr='Sergiy Babenkov';" + "DELETE FROM score WHERE usr='Nadia Brown';" + "DELETE FROM score WHERE usr='Matthew Dunn';" ;
             stmt.executeUpdate(sql);
+
+            System.out.println("Deleted the test row from the table...");
+
+            //Clean Up Environment
 
             stmt.close();
             conn.close();
