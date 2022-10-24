987998b5ee90427a855dbcf630637dcbcaa3e394
Sergiy Babenkov <sergiy.babenkov@uconn.edu>
2022-10-24 17:24:05 -0400

    Initial Commit

--- src/Databases/H2CreateQuestionsTable.java
+++ src/Databases/H2CreateQuestionsTable.java
@@ -1,36 +1,11 @@
 package Databases;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-/*
- *
- * From https://www.tutorialspoint.com/h2_database/h2_database_jdbc_connection.htm
- *
- */
+import static Databases.H2Commands.*;
 
 public class H2CreateQuestionsTable {
-    // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:~/test"; //
-
-    //  Databases credentials
-    static final String USER = "sa";
-    static final String PASS = "";
-
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
         try {
-            // STEP 1: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-
-            //STEP 2: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
+            connectToH2();
             //STEP 3: Execute a query
             System.out.println("Creating table in given database...");
             stmt = conn.createStatement();
@@ -41,28 +16,11 @@ public class H2CreateQuestionsTable {
                     " PRIMARY KEY ( id ))";
             stmt.executeUpdate(sql);
             System.out.println("Created table in given database...");
-
             // STEP 4: Clean-up environment
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
+            disconnectFromH2();
         } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) stmt.close();
-            } catch (SQLException se2) {
-            } // nothing we can do
-            try {
-                if (conn != null) conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            } //end finally try
-        } //end try
+            System.out.print(e);
+        }
         System.out.println("Goodbye!");
     }
 }
--- src/Server/FirstServerResource.java
+++ src/Server/FirstServerResource.java
@@ -7,45 +7,21 @@ import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import static Databases.H2Commands.*;
-
 public class FirstServerResource extends ServerResource {
     private static String json_string;
-
     public static void main(String[] args) throws Exception {
-        try {
-            connectToH2();
-            System.out.println("Connected database successfully...");
 
-            stmt = conn.createStatement();
-            String sql = "SELECT * FROM QUESTIONS";
-            ResultSet rs = stmt.executeQuery(sql);
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
 
-            while (rs.next()) {
-                // Retrieve by column name
-                String Questions = rs.getString("Question");
-                String Answers = rs.getString("Answer");
+        Questions one_q = new Questions("How many members of the US Congress?", "435");
 
-                GsonBuilder builder = new GsonBuilder();
-                builder.setPrettyPrinting();
-                Gson gson = builder.create();
+        json_string = gson.toJson(one_q);
 
-                Questions one_q = new Questions(Questions, Answers);
-
-                json_string = gson.toJson(one_q);
-            }
-            rs.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
         // Create the HTTP server and listen on port 8182
         new Server(Protocol.HTTP, 8182, FirstServerResource.class).start();
     }
-
     @Get
     public String toString() {
         return json_string;
