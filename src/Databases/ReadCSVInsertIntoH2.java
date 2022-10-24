a9b40b2c9a0622f8aecb1e449196b3e140ae9ab8
Sergiy Babenkov <sergiy.babenkov@uconn.edu>
2022-10-24 17:07:18 -0400

    Initial Commit

--- src/Databases/ReadCSVInsertIntoH2.java
+++ src/Databases/ReadCSVInsertIntoH2.java
@@ -6,46 +6,12 @@ import org.apache.commons.csv.CSVRecord;
 
 import java.io.FileReader;
 import java.io.Reader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
+import static Databases.H2Commands.*;
 
 public class ReadCSVInsertIntoH2 {
     // Data file
     private final static String file = constants.CsvFileLocation;
-    // JDBC driver name and database URL
-    private static final String JDBC_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:~/test"; //
-
-    //  Database credentials
-    private static final String USER = "sa";
-    private static final String PASS = "";
-
-    private static Connection conn = null;
-    private static Statement stmt = null;
-
-    private static void connectToH2() {
-        try {
-            // STEP 1: Register JDBC driver
-            Class.forName(JDBC_DRIVER);
-
-            //STEP 2: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-        } catch (Exception e) {
-            System.err.println("Error! in connectTo H2()");
-        }
-    }
-
-    private static void disconnectFromH2() {
-        try {
-            stmt.close();
-            conn.close();
-        } catch (Exception e) {
-            System.err.println("Error! in disconnectFrom H2()");
-        }
-    }
 
     private static void insertIntoH2(int id, String question, String answer) {
         try {
@@ -58,7 +24,7 @@ public class ReadCSVInsertIntoH2 {
                     answer +
                     "'" +
                     "); ";
-            System.out.println("SQL: " + sql.toString());
+            System.out.println("SQL: " + sql);
 
             stmt.executeUpdate(sql);
             stmt.close();
@@ -67,7 +33,6 @@ public class ReadCSVInsertIntoH2 {
             System.err.println("Error! in insertIntoH2");
         }
     }
-
 
     public static void main(String[] args) {
         try {
--- src/Server/FirstServerResource.java
+++ src/Server/FirstServerResource.java
@@ -3,36 +3,51 @@ package Server;
 import Resources.Questions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.restlet.*;
+import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static Databases.H2Commands.*;
 
 public class FirstServerResource extends ServerResource {
-
-    //private static int count = 0;
     private static String json_string;
 
     public static void main(String[] args) throws Exception {
+        try {
+            connectToH2();
+            System.out.println("Connected database successfully...");
 
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-        Gson gson = builder.create();
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM QUESTIONS";
+            ResultSet rs = stmt.executeQuery(sql);
 
-        Questions one_q = new Questions("How many members of the US Congress?", "435");
+            while (rs.next()) {
+                // Retrieve by column name
+                String Questions = rs.getString("Question");
+                String Answers = rs.getString("Answer");
 
-        json_string = gson.toJson(one_q);
+                GsonBuilder builder = new GsonBuilder();
+                builder.setPrettyPrinting();
+                Gson gson = builder.create();
 
+                Questions one_q = new Questions(Questions, Answers);
 
+                json_string = gson.toJson(one_q);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         // Create the HTTP server and listen on port 8182
         new Server(Protocol.HTTP, 8182, FirstServerResource.class).start();
     }
-
 
     @Get
     public String toString() {
         return json_string;
     }
-
 }
