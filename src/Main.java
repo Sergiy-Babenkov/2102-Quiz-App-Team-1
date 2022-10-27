3d9f7a169b9c1cd890a5a812f59fc5683caf9c37
Sergiy Babenkov <sergiy.babenkov@uconn.edu>
2022-10-27 17:46:45 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import static Server.ServerClass.ServerStart;
+
 public class Main {
     public static void main(String[] args) {
-
+        ServerStart();
     }
 }
--- src/Server/ServerClass.java
+++ src/Server/ServerClass.java
@@ -15,34 +15,38 @@ import static Databases.H2Commands.*;
 public class ServerClass extends ServerResource {
     private static String json_string;
 
-    public static void main(String[] args) throws Exception {
-        // Create the HTTP server and listen on port 8182
-        new Server(Protocol.HTTP, 8182, ServerClass.class).start();
+    public static void ServerStart() {
+        try {
+            // Create the HTTP server and listen on port 8182
+            new Server(Protocol.HTTP, 8182, ServerClass.class).start();
 
-        connectToH2();
-        System.out.println("Connected to the database...");
+            connectToH2();
+            System.out.println("Connected to the database...");
 
-        stmt = conn.createStatement();
-        String sql = "SELECT * FROM Questions ORDER BY RAND() Limit 1";
-        ResultSet rs = stmt.executeQuery(sql);
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM Questions ORDER BY RAND() Limit 1";
+            ResultSet rs = stmt.executeQuery(sql);
 
-        String question = null;
-        String answer = null;
-        while (rs.next()) {
-            int id = rs.getInt("id");
-            question = rs.getString("question");
-            answer = rs.getString("answer");
+            String question = null;
+            String answer = null;
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                question = rs.getString("question");
+                answer = rs.getString("answer");
 
-            GsonBuilder builder = new GsonBuilder();
-            builder.setPrettyPrinting();
-            Gson gson = builder.create();
-            Questions one_q = new Questions(question, answer);
+                GsonBuilder builder = new GsonBuilder();
+                builder.setPrettyPrinting();
+                Gson gson = builder.create();
+                Questions one_q = new Questions(question, answer);
 
-            json_string = gson.toJson(one_q);
+                json_string = gson.toJson(one_q);
+            }
+            System.out.println(json_string);
+            disconnectFromH2();
+            System.out.println("Disconnected from the database...");
+        } catch (Exception e) {
+            System.err.println("Error! in connectTo H2()");
         }
-        System.out.println(json_string);
-        disconnectFromH2();
-        System.out.println("Disconnected from the database...");
     }
     @Get
     public String toString() {
