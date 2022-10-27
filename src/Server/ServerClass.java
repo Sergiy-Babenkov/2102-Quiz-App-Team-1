daa25d24e82000d24bb396fbad67edc5c706f696
Sergiy Babenkov <sergiy.babenkov@uconn.edu>
2022-10-27 16:01:55 -0400

    Initial Commit

--- src/Server/ServerClass.java
+++ src/Server/ServerClass.java
@@ -15,37 +15,35 @@ import static Databases.H2Commands.*;
 public class ServerClass extends ServerResource {
     private static String json_string;
 
+
     public static void main(String[] args) throws Exception {
+        // Create the HTTP server and listen on port 8182
+        new Server(Protocol.HTTP, 8182, ServerClass.class).start();
+
         connectToH2();
         System.out.println("Connected to the database...");
 
         stmt = conn.createStatement();
-        String sql = "SELECT id, question, answer FROM Questions";
+        String sql = "SELECT * FROM Questions ORDER BY RAND() Limit 1";
         ResultSet rs = stmt.executeQuery(sql);
 
+        String question = null;
+        String answer = null;
         while (rs.next()) {
-            // Retrieve by column name
             int id = rs.getInt("id");
-            int age = rs.getInt("age");
-            String first = rs.getString("first");
-            String last = rs.getString("last");
+            question = rs.getString("question");
+            answer = rs.getString("answer");
 
-            // Display values
-            System.out.print("ID: " + id);
-            System.out.print(", Age: " + age);
-            System.out.print(", First: " + first);
-            System.out.println(", Last: " + last);
+            GsonBuilder builder = new GsonBuilder();
+            builder.setPrettyPrinting();
+            Gson gson = builder.create();
+            Questions one_q = new Questions(question, answer);
+
+            json_string = gson.toJson(one_q);
         }
-
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-        Gson gson = builder.create();
-
-        Questions one_q = new Questions("How many members of the US Congress?", "435");
-
-        json_string = gson.toJson(one_q);
-        // Create the HTTP server and listen on port 8182
-        new Server(Protocol.HTTP, 8182, ServerClass.class).start();
+        System.out.println(json_string);
+        disconnectFromH2();
+        System.out.println("Disconnected from the database...");
     }
     @Get
     public String toString() {
