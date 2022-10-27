12e2ebd747a11449d273edc0a8be47f0ca543489
Sergiy Babenkov <sergiy.babenkov@uconn.edu>
2022-10-26 20:28:54 -0400

    Initial Commit

--- src/Server/ServerClass.java
+++ src/Server/ServerClass.java
@@ -8,10 +8,34 @@ import org.restlet.data.Protocol;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+import java.sql.ResultSet;
+
+import static Databases.H2Commands.*;
+
 public class ServerClass extends ServerResource {
     private static String json_string;
 
     public static void main(String[] args) throws Exception {
+        connectToH2();
+        System.out.println("Connected to the database...");
+
+        stmt = conn.createStatement();
+        String sql = "SELECT id, question, answer FROM Questions";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while (rs.next()) {
+            // Retrieve by column name
+            int id = rs.getInt("id");
+            int age = rs.getInt("age");
+            String first = rs.getString("first");
+            String last = rs.getString("last");
+
+            // Display values
+            System.out.print("ID: " + id);
+            System.out.print(", Age: " + age);
+            System.out.print(", First: " + first);
+            System.out.println(", Last: " + last);
+        }
 
         GsonBuilder builder = new GsonBuilder();
         builder.setPrettyPrinting();
@@ -20,7 +44,6 @@ public class ServerClass extends ServerResource {
         Questions one_q = new Questions("How many members of the US Congress?", "435");
 
         json_string = gson.toJson(one_q);
-
         // Create the HTTP server and listen on port 8182
         new Server(Protocol.HTTP, 8182, ServerClass.class).start();
     }
