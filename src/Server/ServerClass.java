00e2be0636b656321b6a6a14b563a1bdecdf6454
Dunncorp <92400666+Dunncorp@users.noreply.github.com>
2022-11-14 00:01:28 -0500

    Update ServerClass.java
    Added Bradford changes to server class

--- src/Server/ServerClass.java
+++ src/Server/ServerClass.java
@@ -1,55 +1,105 @@
 package Server;
 
-import Resources.Questions;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.restlet.Server;
+
+import java.io.IOException;
+import java.io.ObjectInputFilter.Status;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.restlet.*;
+import org.restlet.data.Form;
 import org.restlet.data.Protocol;
+//import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.Put;
+import org.restlet.resource.Resource;
 import org.restlet.resource.ServerResource;
 
-import java.sql.ResultSet;
+import com.fasterxml.jackson.core.JsonParser;
+import com.google.gson.Gson; 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
-import static Databases.H2Commands.*;
+import Resources.Questions;  
 
-public class ServerClass extends ServerResource {
-    private static String json_string;
 
-    public static void ServerStart() {
-        try {
-            // Create the HTTP server and listen on port 8182
-            new Server(Protocol.HTTP, 8182, ServerClass.class).start();
+public class ServerClass extends ServerResource {  
+	
+   private static int count = 0;
+   private static String json_string;
+   private static ArrayList<Questions> allQuestionsAnswers = null;
+   private static GsonBuilder builder = new GsonBuilder();     
+   private static Gson gson = builder.create(); 
+   
+   public static void main(String[] args) throws Exception {  
+	   
+	   builder.setPrettyPrinting();
+	   
+	   AllQuestions allQuestions = new AllQuestions();
+	   
+	   allQuestionsAnswers = allQuestions.getAllQuestionsAnswers();
+	   
+       // Create the HTTP server and listen on port 8182 
+	  new Server(Protocol.HTTP, 8182, ServerClass.class).start();    
+   }
+   
+   /*
+    * curl http://localhost:8182
+    */
+   @Get  
+   public String toString() {  
+	  count++;
+	  count = count % allQuestionsAnswers.size();
+	  Questions questionAnswer = allQuestionsAnswers.get(count);
+	  json_string = gson.toJson(questionAnswer);
+      return  json_string;  // "Question " + Integer.valueOf(count).toString() + ": " +
+   }
+  
+   
+   /*
+    * curl -v -d "Hello=YEAH" -X  POST "http://localhost:8182" -H "Content-type: text/html; charset=UTF-8"
+    */
+   @Post
+   public Response addScore(Representation r) {
+	   String s;
+	   Request request = new Request();
+	
+	   try {
+		s = r.getText();
+		System.out.println("s: " + s);
+	   } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	   }
 
-            connectToH2();
-            System.out.println("Connected to the database...");
-
-            stmt = conn.createStatement();
-            String sql = "SELECT * FROM Questions ORDER BY RAND() Limit 1";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            String question = null;
-            String answer = null;
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                question = rs.getString("question");
-                answer = rs.getString("answer");
-
-                GsonBuilder builder = new GsonBuilder();
-                builder.setPrettyPrinting();
-                Gson gson = builder.create();
-                Questions one_q = new Questions(question, answer);
-
-                json_string = gson.toJson(one_q);
-            }
-            System.out.println(json_string);
-            disconnectFromH2();
-            System.out.println("Disconnected from the database...");
-        } catch (Exception e) {
-            System.err.println("Error! in connectTo H2()");
-        }
-    }
-    @Get
-    public String toString() {
-        return json_string;
-    }
+	   Response response = new Response(request);
+	   return response;
+   }
+	
+   /*
+    * curl -v -d "Hello=YEAH" -X  PUT "http://localhost:8182" -H "Content-type: text/html; charset=UTF-8"
+    */
+   @Put
+   public Response updateScore(Representation r) {
+       String s;
+	   
+	try {
+		s = r.getText();
+		System.out.println("s: " + s);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	Request request = new Request();
+	Response response = new Response(request);
+       
+    return response;
+   }
+ 
 }
